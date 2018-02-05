3b384db2d7174b9ce407696685ca1f488ed22d1c
David Walters <25425127+dw-versent@users.noreply.github.com>
2018-02-05 04:23:32 +0000

    special src

---
+++ src/main/groovy/net/tqxr/CucumberWiremockApplication.groovy
@@ -0,0 +1,12 @@
+package net.tqxr
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class CucumberWiremockApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run CucumberWiremockApplication, args
+	}
+}
